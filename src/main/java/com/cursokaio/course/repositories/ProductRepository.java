e3af03da6e6dcc74b66d8009c7e0d7e538a82062
KaioRapanos <kaiorapanos@gmail.com>
2023-05-31 21:15:24 -0300

    Product

---
+++ src/main/java/com/cursokaio/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.cursokaio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursokaio.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+
+}
